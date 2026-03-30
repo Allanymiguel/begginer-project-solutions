7721ecae89b2ee16d4e51284b819241ce3d17447
Allany Miguel Ferreira da Silva <aallanymiguel@gmail.com>
2026-03-30 18:23:49 -0300

    feat: menu to keep answering implemented

--- src/challenge/MagicEightBall.java
+++ src/challenge/MagicEightBall.java
@@ -4,18 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class MagicEightBall {
-    static void main() {
+    static void main() throws InterruptedException {
 
         Scanner scan = new Scanner(System.in);
         List<String> answers = listOfAnswers();
         Random randomNumber = new Random();
+        boolean running = true;
 
-        System.out.println("What is your question?");
-        String question = scan.nextLine();
+        while(running){
+            System.out.println("What is your question?");
+            scan.nextLine();
+            System.out.println("Humm... thinking...\n");
+            TimeUnit.SECONDS.sleep(3);
+            System.out.println(answers.get(randomNumber.nextInt(19)));
+            TimeUnit.SECONDS.sleep(1);
 
-        System.out.println(answers.get(randomNumber.nextInt(19)));
+            System.out.println("Press number 1 to ask something or any other key to quit: ");
+            String option = scan.next();
+            scan.nextLine();
+
+            if(option.equals("1")) continue;
+            else {
+                System.out.println("Finishing application.");
+                running = false;
+            }
+
+        }
 
         scan.close();
 
