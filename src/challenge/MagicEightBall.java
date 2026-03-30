7eebf3058b2aad3a9e778f08d404bdfdf940e94b
Allany Miguel Ferreira da Silva <aallanymiguel@gmail.com>
2026-03-30 17:36:39 -0300

    feat: 20 questions created

---
+++ src/challenge/MagicEightBall.java
@@ -0,0 +1,46 @@
+package challenge;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MagicEightBall {
+    static void main() {
+        Scanner scan = new Scanner(System.in);
+
+        List<String> answers = listOfAnswers();
+
+        System.out.println("What is your question?");
+        String question = scan.nextLine();
+
+
+
+    }
+
+    public static List<String> listOfAnswers() {
+        List<String> ans = new ArrayList<>();
+        ans.add("It is certain.");
+        ans.add("Reply hazy, try again.");
+        ans.add("It is decidedly so.");
+        ans.add("It is certain.");
+        ans.add("Most likely.");
+        ans.add("Concentrate and ask again.");
+        ans.add("As I see it, yes.");
+        ans.add("My reply is no.");
+        ans.add("Without a doubt.");
+        ans.add("Yes definitely.");
+        ans.add("Ask again later.");
+        ans.add("Yes.");
+        ans.add("My source says no.");
+        ans.add("Cannot predict now.");
+        ans.add("Sign points to yes.");
+        ans.add("Outlook not so good.");
+        ans.add("Better not tell you now...");
+        ans.add("You may not rely on it.");
+        ans.add("Very doubtful.");
+        ans.add("Don't count on it.");
+
+        return ans;
+    }
+
+}
