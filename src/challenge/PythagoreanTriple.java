dbc781e3836a706838d05cf11fc908dd710aba56
Allany Miguel Ferreira da Silva <aallanymiguel@gmail.com>
2026-03-30 19:03:42 -0300

    feat: variables to store user inputs

---
+++ src/challenge/PythagoreanTriple.java
@@ -0,0 +1,20 @@
+package challenge;
+
+import java.util.Scanner;
+
+public class PythagoreanTriple {
+    static void main() {
+        Scanner scan = new Scanner(System.in);
+        boolean isPythagorean = false;
+
+        System.out.println("Inform the sides of the triangle: ");
+        Double a = scan.nextDouble();
+        Double b = scan.nextDouble();
+        Double c = scan.nextDouble();
+
+        if ((a*a + b*b) == (c*c)){
+            
+        }
+
+    }
+}
