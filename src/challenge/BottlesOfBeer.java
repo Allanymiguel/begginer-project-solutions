0dada49b66e9edaa0e2ce4d8e0bbcbcb3341aed6
Allany Miguel Ferreira da Silva <aallanymiguel@gmail.com>
2026-03-30 17:13:27 -0300

    correct value of beers assigned

--- src/challenge/BottlesOfBeer.java
+++ src/challenge/BottlesOfBeer.java
@@ -3,15 +3,14 @@ package challenge;
 public class BottlesOfBeer {
     static void main() {
 
-        int beers = 9;
-        boolean running = true;
+        int beers = 99;
 
-        while(running) {
+        while(true) {
             System.out.println(beers +" bottles of beer on the wall, " +(beers--) +" bottles of beer.");
             if(beers == 1){
                 System.out.println("Take one down and pass it around, " +beers
                                     +" bottle of beer on the wall.");
-                running = false; break;
+                break;
             }
             System.out.println("Take one down and pass it around, " +beers +" bottles of beer on the wall.\n");
         }
